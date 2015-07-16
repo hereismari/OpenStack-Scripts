9213b509e806c31c04ff02ce9706b7473d666fb4
mari-linhares <marianne.monteiro@ccc.ufcg.edu.br>
2015-07-16 13:42:50 -0300

    Commiting script

---
+++ scriptSwift.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
+# Ziping a directory and moving it to openstack swift
+
+cd path_to_your_folder
+source  your_project-openrc.sh
+CONTAINER="Your Container Name"
+
+FOLDER="name of your folder"
+zip -r "${FOLDER}" "${FOLDER}"
+TARGET="${FOLDER}.zip"
+
+swift upload $CONTAINER $TARGET
+
