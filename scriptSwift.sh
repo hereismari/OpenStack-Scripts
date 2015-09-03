a6de6697a42d17fd624998b31a8b1800fbc063f1
Marianne Linhares Monteiro <marianne.monteiro@ccc.ufcg.edu.br>
2015-09-03 10:46:52 -0300

    Update scriptSwift.sh

--- scriptSwift.sh
+++ scriptSwift.sh
@@ -3,7 +3,7 @@
 # Ziping a directory and moving it to openstack swift
 
 cd path_to_your_folder
-source  your_project-openrc.sh
+source your_project-openrc.sh
 CONTAINER="Your Container Name"
 
 FOLDER="name of your folder"
